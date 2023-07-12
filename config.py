f990d7d6743817693daec0e8534da69d087cb323
ArcaneSK <silentkill@gmail.com>
2023-07-12 17:29:33 -0500

    Add env variable check for smart and dumb chat models

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ class Config():
         self.database_name = os.getenv("DATABASE_NAME")
 
         self.open_api_key = os.getenv("OPENAI_API_KEY")
-        self.smart_cli_model = "gpt-4"
-        self.dumb_cli_model = "gpt-3.5-turbo"
+        self.smart_cli_model = os.getenv("OPENAI_CHAT_SMART_MODEL", "gpt-4")
+        self.dumb_cli_model = os.getenv("OPENAI_CHAT_DUMB_MODEL", "gpt-3.5-turbo")
 
         self.default_system_prompt = "You are a helpful AI chat bot."
         self.filename_prompt = "What's a good filename for this conversation, based on user " \
